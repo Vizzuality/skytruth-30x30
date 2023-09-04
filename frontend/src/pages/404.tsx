b585e4b98137898fd8b520957ee8898e767f64a0
Clément Prod'homme <contact@clementprodhomme.fr>
2023-09-04 09:30:06 +0200

    Style the error pages

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Link from 'next/link';
+
+import { ArrowLeft } from 'lucide-react';
 
 import { DefaultLayout } from '@/layouts/default';
 
@@ -7,7 +10,19 @@ const Home: React.FC = () => (
     <Head>
       <title>Page Not Found</title>
     </Head>
-    <p>404</p>
+    <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+      <p className="text-base font-semibold leading-8">404</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Page Not Found</h1>
+      <p className="mt-4 text-base sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
+      <div className="mt-10 flex justify-center">
+        <Link
+          href="/"
+          className="rounded-lg text-sm font-semibold leading-7 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
+        >
+          <ArrowLeft className="mr-2 inline-block h-6 w-6" aria-hidden /> Back to home
+        </Link>
+      </div>
+    </div>
   </DefaultLayout>
 );
 
--- frontend/src/pages/500.tsx
+++ frontend/src/pages/500.tsx
@@ -1,13 +1,28 @@
 import Head from 'next/head';
+import Link from 'next/link';
+
+import { ArrowLeft } from 'lucide-react';
 
 import { DefaultLayout } from '@/layouts/default';
 
 const Home: React.FC = () => (
   <DefaultLayout>
     <Head>
-      <title>Error</title>
+      <title>Internal Server Error</title>
     </Head>
-    <p>500</p>
+    <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+      <p className="text-base font-semibold leading-8">500</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Internal Server Error</h1>
+      <p className="mt-4 text-base sm:mt-6">Sorry, something went wrong.</p>
+      <div className="mt-10 flex justify-center">
+        <Link
+          href="/"
+          className="rounded-lg text-sm font-semibold leading-7 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
+        >
+          <ArrowLeft className="mr-2 inline-block h-6 w-6" aria-hidden /> Back to home
+        </Link>
+      </div>
+    </div>
   </DefaultLayout>
 );
 
