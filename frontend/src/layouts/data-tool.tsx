74c2a83cf6633836867c70adb28a29799a71917e
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-10-17 13:48:25 +0100

    Add base data-tool layout (copy of fullscreen one)

---
+++ frontend/src/layouts/data-tool.tsx
@@ -0,0 +1,31 @@
+import { PropsWithChildren } from 'react';
+
+import Head from 'next/head';
+
+import Header from '@/components/header';
+
+export interface FullscreenLayoutProps {
+  title?: string;
+  description?: string;
+}
+
+const FullscreenLayout: React.FC<PropsWithChildren<FullscreenLayoutProps>> = ({
+  children,
+  title,
+  description,
+}) => (
+  <>
+    <Head>
+      <title>{`${title ? `${title} | ` : ''}Skytruth 30x30`}</title>
+      {description && <meta name="description" content={description} />}
+    </Head>
+    <div className="flex h-screen w-screen flex-col">
+      <div className="flex-shrink-0">
+        <Header />
+      </div>
+      <div className="relative flex h-full w-full flex-col md:flex-row">{children}</div>
+    </div>
+  </>
+);
+
+export default FullscreenLayout;
