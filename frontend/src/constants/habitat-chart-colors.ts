5806e2e70c3b1096d6aa01bf0a0dcfe2a14e36a6
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-10-20 11:05:58 +0100

    Add mapping between habitats and colors for use in widgets

---
+++ frontend/src/constants/habitat-chart-colors.ts
@@ -0,0 +1,8 @@
+export const HABITAT_CHART_COLORS = {
+  'warm-water corals': '#AD6CFF',
+  'cold-water corals': '#04D8F4',
+  mangroves: '#FD8E28',
+  seagrasses: '#02B07C',
+  saltmarshes: '#717B00',
+  seamounts: '#9B5400',
+};
