54737de6fa70ede4c111fcd57d8c696692218e4d
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-11-02 12:48:10 +0000

    DataTool content details pane to use available settings and interpolate title as well to choose the correct table to display

--- frontend/src/containers/data-tool/content/details/index.tsx
+++ frontend/src/containers/data-tool/content/details/index.tsx
@@ -1,36 +1,47 @@
-import DashboardTable from '@/components/dashboard-table';
-import { Button } from '@/components/ui/button';
-import { useSyncDataToolContentSettings } from '@/containers/data-tool/sync-settings';
+import { useMemo } from 'react';
 
-import { mockedData } from './mocked-data';
+import { useAtomValue } from 'jotai';
+
+import { Button } from '@/components/ui/button';
+import tablesSettings from '@/containers/data-tool/content/details/tables-settings';
+import { useSyncDataToolContentSettings } from '@/containers/data-tool/sync-settings';
+import { locationAtom } from '@/store/location';
 
 const DataToolDetails: React.FC = () => {
   const [, setSettings] = useSyncDataToolContentSettings();
+  const location = useAtomValue(locationAtom);
 
   const handleOnCloseClick = () => {
     setSettings((prevSettings) => ({ ...prevSettings, showDetails: false }));
   };
 
+  const table = useMemo(() => {
+    // TODO: Improve to support more entries (although not needed right now)
+    const tableSettings = tablesSettings.worldwideRegion.locationTypes.includes(location.type)
+      ? tablesSettings.worldwideRegion
+      : tablesSettings.countryHighseas;
+
+    const parsedTitle = tableSettings.title[location.type].replace('{location}', location.name);
+
+    return {
+      title: parsedTitle,
+      component: tableSettings.component,
+    };
+  }, [location]);
+
   return (
     <div className="absolute h-full w-full overflow-scroll bg-white px-4 py-4 md:px-6">
       <div className="mb-8 flex gap-8 md:justify-between">
         <span className="max-w-lg">
-          <h2 className="text-4xl font-extrabold">
-            Marine Conservation at National and Regional Levels
-          </h2>
+          <h2 className="text-4xl font-extrabold">{table.title}</h2>
         </span>
         <Button variant="text-link" className="m-0 cursor-pointer p-0" onClick={handleOnCloseClick}>
           Close
         </Button>
       </div>
       <div className="overflow-scroll">
-        {/* NOTE: Div to test the horizontal overflow */}
-        <div className="mt-4" style={{ width: '130%' }}>
-          {/*
-            NOTE: We don't need a separate table component, but it's easier
-            to have one while using mocked data in order to keep this component clean
-          */}
-          <DashboardTable data={mockedData} />
+        <div className="mt-4">
+          <table.component />
         </div>
       </div>
     </div>
