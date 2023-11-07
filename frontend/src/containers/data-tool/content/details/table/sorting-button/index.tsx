62a3bb98f1f518cc82b852fd9ebb00b3aedbf9cd
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-11-07 12:53:46 +0000

    Use alternative lucide-react icons for table sorting

--- frontend/src/containers/data-tool/content/details/table/sorting-button/index.tsx
+++ frontend/src/containers/data-tool/content/details/table/sorting-button/index.tsx
@@ -1,8 +1,10 @@
 import { Column } from '@tanstack/react-table';
-import { ArrowDownAZ, ArrowDownUp, ArrowDownZA } from 'lucide-react';
+import { ArrowDownNarrowWide, ArrowUpNarrowWide, ArrowUpDown } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { GlobalRegionalTableColumns } from '@/containers/data-tool/content/details/tables/global-regional/columns';
+
+const ICON_CLASSNAMES = 'h-4 w-4';
 
 type SortingButtonProps = {
   column: Column<GlobalRegionalTableColumns, unknown>;
@@ -16,19 +18,19 @@ const SortingButton: React.FC<SortingButtonProps> = ({ column }) => {
       {!isSorted && (
         <Button size="icon" variant="ghost" onClick={() => column.toggleSorting(false)}>
           <span className="sr-only">Sort ascending</span>
-          <ArrowDownUp className="h-5 w-5" aria-hidden />
+          <ArrowUpDown className={ICON_CLASSNAMES} aria-hidden />
         </Button>
       )}
       {isSorted === 'asc' && (
         <Button size="icon" variant="ghost" onClick={() => column.toggleSorting(true)}>
           <span className="sr-only">Sort descending</span>
-          <ArrowDownAZ className="h-5 w-5" aria-hidden />
+          <ArrowDownNarrowWide className={ICON_CLASSNAMES} aria-hidden />
         </Button>
       )}
       {isSorted === 'desc' && (
         <Button size="icon" variant="ghost" onClick={() => column.clearSorting()}>
           <span className="sr-only">Clear sorting</span>
-          <ArrowDownZA className="h-5 w-5" aria-hidden />
+          <ArrowUpNarrowWide className={ICON_CLASSNAMES} aria-hidden />
         </Button>
       )}
     </>
