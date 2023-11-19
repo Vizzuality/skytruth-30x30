a91d6ac77c3cb1c1cab362da34767478ade49132
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-11-19 14:27:47 +0000

    Add Homepage placeholder component to display the Earth surface coverage chart

---
+++ frontend/src/containers/homepage/earth-surface-coverage/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+const EarthSurfaceCoverage: React.FC = () => (
+  <div className="relative -mt-[80px] flex justify-center">
+    <Image
+      className="h-auto w-full max-w-4xl"
+      src="/images/homepage/earth-surface-coverage.svg"
+      alt="Earth surface coverage"
+      width="0"
+      height="0"
+      sizes="100vw"
+      priority
+    />
+  </div>
+);
+
+export default EarthSurfaceCoverage;
