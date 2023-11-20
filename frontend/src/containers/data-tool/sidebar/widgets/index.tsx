09c516d72b9796a7f9018d6031d6a2fd48d922b8
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-11-20 11:14:05 +0000

    Add the Protection Types widget to the data tool

--- frontend/src/containers/data-tool/sidebar/widgets/index.tsx
+++ frontend/src/containers/data-tool/sidebar/widgets/index.tsx
@@ -4,6 +4,7 @@ import { locationAtom } from '@/store/location';
 
 import HabitatWidget from './habitat';
 import MarineConservationWidget from './marine-conservation';
+import ProtectionTypesWidget from './protection-types';
 
 const DataToolWidgets: React.FC = () => {
   const location = useAtomValue(locationAtom);
@@ -11,6 +12,7 @@ const DataToolWidgets: React.FC = () => {
   return (
     <div className="flex flex-col divide-y-[1px] divide-black font-mono">
       <MarineConservationWidget location={location} />
+      <ProtectionTypesWidget location={location} />
       <HabitatWidget location={location} />
     </div>
   );
---
+++ frontend/src/containers/data-tool/sidebar/widgets/protection-types/index.tsx
@@ -0,0 +1,88 @@
+import { useMemo } from 'react';
+
+import HorizontalBarChart from '@/components/charts/horizontal-bar-chart';
+import Widget from '@/components/widget';
+import { PROTECTION_TYPES_CHART_COLORS } from '@/constants/protection-types-chart-colors';
+import { useGetMpaaProtectionLevelStats } from '@/types/generated/mpaa-protection-level-stat';
+import type { Location } from '@/types/generated/strapi.schemas';
+
+type ProtectionTypesWidgetProps = {
+  location: Location;
+};
+
+const ProtectionTypesWidget: React.FC<ProtectionTypesWidgetProps> = ({ location }) => {
+  // Default params: filter by location
+  const defaultQueryParams = {
+    filters: {
+      location: {
+        code: location.code,
+      },
+    },
+  };
+
+  // Find last updated in order to display the last data update
+  const { data: dataLastUpdate } = useGetMpaaProtectionLevelStats(
+    {
+      ...defaultQueryParams,
+      sort: 'updatedAt:desc',
+      'pagination[limit]': 1,
+    },
+    {
+      query: {
+        select: ({ data }) => data?.[0]?.attributes?.updatedAt,
+        placeholderData: { data: null },
+      },
+    }
+  );
+
+  // Get protection levels by location
+  const {
+    data: { data: protectionLevelStatsData },
+  } = useGetMpaaProtectionLevelStats(
+    {
+      ...defaultQueryParams,
+      populate: '*',
+      'pagination[limit]': -1,
+    },
+    {
+      query: {
+        select: ({ data }) => ({ data }),
+        placeholderData: { data: [] },
+      },
+    }
+  );
+
+  // Parse data to display in the chart
+  const widgetChartData = useMemo(() => {
+    if (!protectionLevelStatsData.length) return [];
+
+    const parsedData = protectionLevelStatsData.map((entry) => {
+      const stats = entry?.attributes;
+      const protectionLevel = stats?.mpaa_protection_level?.data.attributes;
+
+      return {
+        title: protectionLevel.name,
+        slug: protectionLevel.slug,
+        barColor: PROTECTION_TYPES_CHART_COLORS[protectionLevel.slug],
+        totalArea: location.totalMarineArea,
+        protectedArea: stats.area,
+        info: protectionLevel.info,
+      };
+    });
+
+    return parsedData;
+  }, [location, protectionLevelStatsData]);
+
+  // If there's no widget data, don't display the widget
+  if (!widgetChartData.length) return null;
+
+  return (
+    <Widget title="Marine Conservation Protection Types" lastUpdated={dataLastUpdate}>
+      {widgetChartData.map((chartData) => (
+        <HorizontalBarChart key={chartData.slug} className="py-2" data={chartData} />
+      ))}
+    </Widget>
+  );
+};
+
+export default ProtectionTypesWidget;
