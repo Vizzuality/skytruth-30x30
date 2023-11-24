3bda33b378939b3f37fe5210330e6b14df2258d8
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-11-24 10:06:45 +0000

    Use Types from next/image for the Logo type in the logos grid component

--- frontend/src/containers/about/logos-grid/index.tsx
+++ frontend/src/containers/about/logos-grid/index.tsx
@@ -8,27 +8,17 @@ type LogosGridProps = {
 
 const LogosGrid: React.FC<LogosGridProps> = ({ type }) => (
   <div className="grid gap-4 md:grid-cols-4">
-    {LOGOS[type].map(({ logo, alt, link, description, dimensions }) => (
+    {LOGOS[type].map(({ logo, alt, link, description, width, height }) => (
       <div key={logo} className="flex flex-col gap-4 pr-4">
         <span className="flex flex-1 items-center justify-center md:justify-start">
           {link && (
             <a target="_blank" href={link} rel="noopener noreferrer">
-              <Image
-                src={`${LOGOS_PATH}${logo}`}
-                alt={alt}
-                width={dimensions[0]}
-                height={dimensions[1]}
-              />
+              <Image src={`${LOGOS_PATH}${logo}`} alt={alt} width={width} height={height} />
             </a>
           )}
           {!link && (
             <span>
-              <Image
-                src={`${LOGOS_PATH}${logo}`}
-                alt={alt}
-                width={dimensions[0]}
-                height={dimensions[1]}
-              />
+              <Image src={`${LOGOS_PATH}${logo}`} alt={alt} width={width} height={height} />
             </span>
           )}
         </span>
--- frontend/src/containers/about/logos-grid/types.ts
+++ frontend/src/containers/about/logos-grid/types.ts
@@ -1,9 +1,9 @@
-export type LogoType = {
+import { ImageProps } from 'next/image';
+
+export type LogoType = Pick<ImageProps, 'alt' | 'width' | 'height'> & {
   logo: string;
-  alt: string;
   link?: string;
   description?: string;
-  dimensions: [number, number];
 };
 
 export type LogosType = {
