d3357670a9dc9662a21c3df155708a2194834d6c
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-11-28 16:14:21 +0000

    Add CTAs to ToS and Privacy pages

--- frontend/src/pages/terms-of-use.tsx
+++ frontend/src/pages/terms-of-use.tsx
@@ -1,8 +1,22 @@
+import Cta from '@/components/static-pages/cta';
 import Section from '@/components/static-pages/section';
+import { PAGES } from '@/constants/pages';
 import Layout, { Content } from '@/layouts/static-page';
 
 const PrivacyPolicy: React.FC = () => (
-  <Layout title="Terms of Use">
+  <Layout
+    title="Terms of Use"
+    bottom={
+      <Cta
+        title="Take action."
+        description="Do you want to contribute to the Knowledge Hub and add a tool?"
+        button={{
+          text: 'Go to data tool',
+          link: PAGES.dataTool,
+        }}
+      />
+    }
+  >
     <Content>
       <h1 className="text-5xl font-extrabold leading-tight md:text-6xl">Terms of Use</h1>
       <Section borderTop={false}>
