d60fd37a0f9472ce08e46a105d7157bffe1a3c5d
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-11-28 10:56:14 +0000

    Fix Marine Conservation Establishment Stages widget bars missing backgrounds

--- frontend/src/constants/establishment-stages-chart-bar-backgrounds.ts
+++ frontend/src/constants/establishment-stages-chart-bar-backgrounds.ts
@@ -1,7 +1,10 @@
 import { BAR_BACKGROUNDS } from '@/components/charts/horizontal-bar-chart/constants';
 
 export const ESTABLISHMENT_STAGES_CHART_BAR_BACKGROUNDS = {
-  'designated-implemented': 'dots',
-  'designated-unimplemented': 'crosses',
-  'propose-committed': 'arrows',
+  'proposed-committed': 'dots',
+  implemented: 'crosses',
+  designated: 'arrows',
+  unknown: 'dashes',
+  'actively-managed': 'dashes',
+  'designated-unimplemented': 'arrows',
 } satisfies Record<string, keyof typeof BAR_BACKGROUNDS>;
