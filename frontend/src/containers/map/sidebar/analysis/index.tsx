2c5b1886707293cd48fde6d2747a5c73448bf3c7
Simao Rodrigues <arodrigues.simao@gmail.com>
2023-12-05 13:19:46 +0000

    Add back button to Analysis sidebar

--- frontend/src/containers/map/sidebar/analysis/index.tsx
+++ frontend/src/containers/map/sidebar/analysis/index.tsx
@@ -4,6 +4,7 @@ import { analysisAtom } from '../../store';
 
 import AnalysisButtons from './analysis-buttons';
 import AnalysisIntro from './analysis-intro';
+import BackButton from './back-button';
 import AnalysisWidget from './widget';
 
 const SidebarAnalysis: React.FC = () => {
@@ -13,16 +14,21 @@ const SidebarAnalysis: React.FC = () => {
   const showButtons = ['success', 'error'].includes(analysisStatus);
 
   return (
-    <div className="h-full w-full overflow-y-scroll border-x border-black pb-12">
-      <div className="border-b border-black px-4 pt-4 pb-2 md:px-8">
-        <h1 className="text-5xl font-black">
-          {showIntro ? 'Marine Conservation Modelling' : 'Custom Area'}
-        </h1>
-        <div className="my-2">{showButtons && <AnalysisButtons />}</div>
+    <>
+      <div className="h-full w-full overflow-y-scroll border-x border-black pb-12">
+        <div className="border-b border-black px-4 pt-4 pb-2 md:px-8">
+          <h1 className="text-5xl font-black">
+            {showIntro ? 'Marine Conservation Modelling' : 'Custom Area'}
+          </h1>
+          <div className="my-2">{showButtons && <AnalysisButtons />}</div>
+        </div>
+        {showIntro && <AnalysisIntro />}
+        {!showIntro && <AnalysisWidget />}
       </div>
-      {showIntro && <AnalysisIntro />}
-      {!showIntro && <AnalysisWidget />}
-    </div>
+      <div className="absolute bottom-0 left-px">
+        <BackButton />
+      </div>
+    </>
   );
 };
 
