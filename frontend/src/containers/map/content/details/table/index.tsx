5ad45d7a96cd2999ec8d6f256bf403177088463b
Andrés González <andres.gonzalez@vizzuality.com>
2024-01-30 18:20:01 +0100

    applies border-left instead of right

--- frontend/src/containers/map/content/details/table/index.tsx
+++ frontend/src/containers/map/content/details/table/index.tsx
@@ -24,19 +24,21 @@ const MapTable = ({ columns, data }) => {
   const hasData = table.getRowModel().rows?.length > 0;
 
   const firstColumn = columns[0];
+  const secondColumn = columns[1];
   const lastColumn = columns[columns.length - 1];
 
   return (
     <table
       ref={tableRef}
-      className="relative border-separate border-spacing-0 whitespace-nowrap pr-6 font-mono text-xs"
+      className="relative border-spacing-0 whitespace-nowrap pr-6 font-mono text-xs"
     >
       <thead className="sticky -top-4 z-10 bg-white text-left">
         {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
+          <tr key={headerGroup.id} className="shadow-[0_2px_0_-1px_rgb(0,0,0)]">
             {headerGroup.headers.map((header) => {
-              const { id, column } = header;
+              const { id, column, index } = header;
               const isFirstColumn = id === firstColumn.accessorKey;
+              const isSecondColumn = index === 1;
               const isLastColumn = id === lastColumn.accessorKey;
 
               return (
@@ -44,8 +46,9 @@ const MapTable = ({ columns, data }) => {
                   key={id}
                   ref={isFirstColumn ? firstColumnRef : null}
                   className={cn({
-                    'h-10 border-b border-black py-3 pl-6 pr-16': true,
-                    'details-table-first-column-border border-black pl-0 pr-5': isFirstColumn,
+                    'h-10 py-3 pl-6 pr-16': true,
+                    'pl-0 pr-5': isFirstColumn,
+                    'border-l border-dashed border-black': isSecondColumn,
                     'pr-0': isLastColumn,
                   })}
                 >
@@ -60,18 +63,26 @@ const MapTable = ({ columns, data }) => {
         {hasData &&
           table.getRowModel().rows.map((row) => {
             return (
-              <tr key={row.id} className="border-b border-t border-black">
+              <tr
+                key={row.id}
+                className={cn({
+                  'border-b border-t border-black': true,
+                  'border-t-0': row.index === 0,
+                })}
+              >
                 {row.getVisibleCells().map((cell) => {
                   const { column } = cell;
                   const isFirstColumn = column.id === firstColumn.accessorKey;
+                  const isSecondColumn = column.id === secondColumn.accessorKey;
                   const isLastColumn = column.id === lastColumn.accessorKey;
 
                   return (
                     <td
                       key={cell.id}
                       className={cn({
-                        'h-16 border-b border-black py-3 pl-6 pr-16': true,
-                        'details-table-first-column-border border-black pl-0 pr-5': isFirstColumn,
+                        'h-16 py-3 pl-6 pr-16': true,
+                        'pl-0 pr-5': isFirstColumn,
+                        'border-l border-dashed border-black': isSecondColumn,
                         'pr-0': isLastColumn,
                       })}
                     >
