f5e87c1889d630005d4f8b6b1ce0a820626daaab
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-02-09 15:51:01 +0000

    Add ContactDetail collection type to Orval config and generate types

--- frontend/src/types/generated/strapi.schemas.ts
+++ frontend/src/types/generated/strapi.schemas.ts
@@ -1054,6 +1054,49 @@ export type GetDataInfosParams = {
   locale?: string;
 };
 
+export type GetContactDetailParams = {
+  /**
+   * Sort by attributes ascending (asc) or descending (desc)
+   */
+  sort?: string;
+  /**
+   * Return page/pageSize (default: true)
+   */
+  'pagination[withCount]'?: boolean;
+  /**
+   * Page number (default: 0)
+   */
+  'pagination[page]'?: number;
+  /**
+   * Page size (default: 25)
+   */
+  'pagination[pageSize]'?: number;
+  /**
+   * Offset value (default: 0)
+   */
+  'pagination[start]'?: number;
+  /**
+   * Number of entities to return (default: 25)
+   */
+  'pagination[limit]'?: number;
+  /**
+   * Fields to return (ex: title,author)
+   */
+  fields?: string;
+  /**
+   * Relations to return
+   */
+  populate?: string;
+  /**
+   * Filters to apply
+   */
+  filters?: { [key: string]: any };
+  /**
+   * Locale to apply
+   */
+  locale?: string;
+};
+
 /**
  * every controller of the api
  */
