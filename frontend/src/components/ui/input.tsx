bae410ec7be07acdbd1d05d5f6eb941b00a5f64f
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-02-15 16:52:53 +0000

    Increase font size for inputs, textarea, select

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full border-b border-black bg-white px-3 py-2 font-mono text-xs ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'flex h-10 w-full border-b border-black bg-white px-3 py-2 font-mono text-base ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
           className
         )}
         ref={ref}
