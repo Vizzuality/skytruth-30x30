f815fae2ec9a02058f19e577159932fb4d07bb13
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-02-16 15:25:42 +0000

    Match back to dashboard button in modelling to the same height used in marine conservation details

--- frontend/src/containers/map/sidebar/analysis/back-button/index.tsx
+++ frontend/src/containers/map/sidebar/analysis/back-button/index.tsx
@@ -17,7 +17,7 @@ const BackButton: React.FC = () => {
 
   return (
     <Button
-      className="flex h-12 justify-between border-l-0 px-5 font-mono text-xs md:px-8"
+      className="flex h-10 justify-between border-l-0 px-5 font-mono text-xs md:px-8"
       variant="blue"
       onClick={onClickBack}
     >
