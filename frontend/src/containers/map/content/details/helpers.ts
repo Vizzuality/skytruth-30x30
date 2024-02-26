918eae31c098b3d3b6637f26a1c5f00bc125d560
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-02-26 12:03:25 +0000

    When no filters are selected, return all data

--- frontend/src/containers/map/content/details/helpers.ts
+++ frontend/src/containers/map/content/details/helpers.ts
@@ -1,6 +1,7 @@
 export const applyFilters = (data, filters) => {
   const filteredData = data.filter((item) => {
     for (const key in filters) {
+      if (!filters[key].length) continue;
       if (!filters[key].includes(item[key])) return false;
     }
     return true;
