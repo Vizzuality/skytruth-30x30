afc972efa98fa8f93f3e2a6933815e0706b83960
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-02-26 14:31:21 +0000

    Clean up unused/commented out code from the LayersToolbox

--- frontend/src/containers/map/content/map/layers-toolbox/index.tsx
+++ frontend/src/containers/map/content/map/layers-toolbox/index.tsx
@@ -1,5 +1,3 @@
-// import { LuChevronDown, LuChevronUp } from 'react-icons/lu';
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import { useSyncMapLayers } from '../sync-settings';
@@ -9,7 +7,6 @@ import Legend from './legend';
 
 const TABS_TRIGGER_CLASSES =
   'group flex flex-1 items-center space-x-1 rounded-none border border-b-0 border-black py-3 px-6 font-mono text-sm font-bold uppercase leading-none text-black last:border-l-0 data-[state=active]:bg-orange';
-// const TABS_ICONS_CLASSES = 'w-6 h-6 -translate-y-[2px]';
 
 const LayersToolbox = (): JSX.Element => {
   const [activeLayers] = useSyncMapLayers();
@@ -21,22 +18,10 @@ const LayersToolbox = (): JSX.Element => {
     >
       <TabsList className="h-auto rounded-none bg-white p-0">
         <TabsTrigger value="layers-list" className={TABS_TRIGGER_CLASSES} tabIndex={0}>
-          <span>layers</span>
-          {/* <LuChevronDown
-            className={`hidden group-data-[state=active]:block ${TABS_ICONS_CLASSES}`}
-          />
-          <LuChevronUp
-            className={`hidden group-data-[state=inactive]:block ${TABS_ICONS_CLASSES}`}
-          /> */}
+          <span>Layers</span>
         </TabsTrigger>
         <TabsTrigger value="legend" className={TABS_TRIGGER_CLASSES} tabIndex={0}>
-          <span>legend</span>
-          {/* <LuChevronDown
-            className={`hidden group-data-[state=active]:block ${TABS_ICONS_CLASSES}`}
-          />
-          <LuChevronUp
-            className={`hidden group-data-[state=inactive]:block ${TABS_ICONS_CLASSES}`}
-          /> */}
+          <span>Legend</span>
         </TabsTrigger>
       </TabsList>
       <div className="w-full overflow-y-scroll border border-black bg-white py-3 px-6">
