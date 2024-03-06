45717b05f3c45067f33b4200dc7431f12443a39b
Agnieszka Figiel <agnieszka.figiel@vizzuality.com>
2024-03-06 10:41:15 +0100

    Added production outputs

--- infrastructure/base/outputs.tf
+++ infrastructure/base/outputs.tf
@@ -24,14 +24,24 @@ output "staging_cms_env" {
   sensitive = true
 }
 
-# output "production_site_url" {
-#   value = module.production.site_url
-# }
+output "production_site_url" {
+  value = module.production.site_url
+}
 
-# output "production_api_url" {
-#   value = module.production.api_url
-# }
+output "production_api_url" {
+  value = module.production.api_url
+}
 
-# output "production_analysis_cloud_function_url" {
-#   value = module.production.analysis_cloud_function_url
-# }
+output "production_analysis_cloud_function_url" {
+  value = module.production.analysis_cloud_function_url
+}
+
+output "production_client_env" {
+  value     = module.production.client_env
+  sensitive = true
+}
+
+output "production_cms_env" {
+  value     = module.production.cms_env
+  sensitive = true
+}
