fa1edb71286c42efc26a72804bc91b3e9509e99e
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-03-11 13:02:23 +0000

    Revert "Ensure that in the global/regional table, area does not exceed the total marine area due to rounding"
    This reverts commit 4ba6019c000ecf286ea73083d1c4dc18aa2fa903.
    
    No longer needed

--- frontend/src/containers/map/content/details/tables/global-regional/useColumns.tsx
+++ frontend/src/containers/map/content/details/tables/global-regional/useColumns.tsx
@@ -15,7 +15,6 @@ import { useMapSearchParams } from '@/containers/map/content/map/sync-settings';
 export type GlobalRegionalTableColumns = {
   location: string;
   locationCode: string;
-  totalMarineArea: number;
   coverage: number;
   locationType: string;
   mpas: number;
@@ -86,20 +85,11 @@ const useColumns = () => {
           </HeaderItem>
         ),
         cell: ({ row }) => {
-          const { totalMarineArea, area: value } = row.original;
+          const { area: value } = row.original;
           const formattedValue = cellFormatter.area(value);
-          let displayValue = formattedValue;
-
-          // In certain circumstances, due to rounding, the formatted value may exceed the
-          // total marine area, mostly in situations where the coverage is nearing 100%.
-          // In this case, we skip the rounding and display the value.
-          if (parseFloat(formattedValue) > totalMarineArea) {
-            displayValue = value.toString();
-          }
-
           return (
             <span>
-              {displayValue} km<sup>2</sup>
+              {formattedValue} km<sup>2</sup>
             </span>
           );
         },
