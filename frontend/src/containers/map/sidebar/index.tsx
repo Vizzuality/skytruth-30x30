22232ad9077aedb9d7278f249abdea0caae6ec7a
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-03-30 16:04:04 +0000

    Work out panels/buttons visibility

--- frontend/src/containers/map/sidebar/index.tsx
+++ frontend/src/containers/map/sidebar/index.tsx
@@ -8,6 +8,8 @@ import { sidebarAtom, layersAtom } from '@/containers/map/store';
 import { cn } from '@/lib/classnames';
 import LayersIcon from '@/styles/icons/layers.svg?sprite';
 
+import { useSyncMapContentSettings } from '../sync-settings';
+
 import Details from './details';
 import Layers from './layers';
 import Modelling from './modelling';
@@ -27,79 +29,97 @@ type MapSidebarProps = {
 };
 
 const MapSidebar: React.FC<MapSidebarProps> = ({ type }) => {
+  const [{ showDetails }] = useSyncMapContentSettings();
   const [isSidebarOpen, setSidebarOpen] = useAtom(sidebarAtom);
   const [isLayersOpen, setLayersOpen] = useAtom(layersAtom);
 
+  // Main panel component selection
   const ContentComponent = SIDEBAR_COMPONENTS[type] || Details;
 
+  // Visibility (main panel/toggle)
+  const showSidebar = true;
+  const showSidebarToggle = !(isSidebarOpen && isLayersOpen) || showDetails;
+
+  // Visibility (layers panel/toggle)
+  const showLayersPanel = !showDetails;
+  const showLayersToggle = showLayersPanel;
+
   return (
-    <div className="relative z-20 flex h-full">
+    <div className="relative z-20 flex h-full border-l border-black">
       {/* MAIN PANEL */}
-      <div
-        className={cn('relative z-10 border-b border-black bg-white', {
-          'border-r': isSidebarOpen,
-        })}
-      >
-        <Collapsible
-          className="h-full overflow-hidden"
-          open={isSidebarOpen}
-          onOpenChange={setSidebarOpen}
+      {showSidebar && (
+        <div
+          className={cn('relative z-10 border-b border-black bg-white', {
+            'border-r': isSidebarOpen,
+          })}
         >
-          <CollapsibleContent className="left-00 relative top-0 h-full flex-shrink-0 bg-white fill-mode-none data-[state=closed]:animate-collapsible-left data-[state=open]:animate-collapsible-right">
-            <div className="h-full md:w-[460px]">
-              <ContentComponent />
-            </div>
-          </CollapsibleContent>
-        </Collapsible>
-      </div>
+          <Collapsible
+            className="h-full overflow-hidden"
+            open={isSidebarOpen}
+            onOpenChange={setSidebarOpen}
+          >
+            <CollapsibleContent className="left-00 relative top-0 h-full flex-shrink-0 bg-white fill-mode-none data-[state=closed]:animate-collapsible-left data-[state=open]:animate-collapsible-right">
+              <div className="h-full md:w-[460px]">
+                <ContentComponent />
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        </div>
+      )}
 
       {/* SECONDARY PANEL (LAYERS) */}
-      <div
-        className={cn('relative z-20 border-b border-black bg-white', { 'border-r': isLayersOpen })}
-      >
-        <Collapsible
-          className="h-full overflow-hidden"
-          open={isLayersOpen}
-          onOpenChange={setLayersOpen}
+      {showLayersPanel && (
+        <div
+          className={cn('relative z-20 border-b border-black bg-white', {
+            'border-r': isLayersOpen,
+          })}
         >
-          <CollapsibleContent className="relative top-0 left-0 h-full flex-shrink-0 bg-white fill-mode-none data-[state=closed]:animate-collapsible-left data-[state=open]:animate-collapsible-right">
-            <div className="h-full md:w-[280px]">
-              <Layers />
-            </div>
-          </CollapsibleContent>
-        </Collapsible>
-      </div>
+          <Collapsible
+            className="h-full overflow-hidden"
+            open={isLayersOpen}
+            onOpenChange={setLayersOpen}
+          >
+            <CollapsibleContent className="relative top-0 left-0 h-full flex-shrink-0 bg-white fill-mode-none data-[state=closed]:animate-collapsible-left data-[state=open]:animate-collapsible-right">
+              <div className="h-full md:w-[280px]">
+                <Layers />
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        </div>
+      )}
 
       {/* BUTTONS */}
       <div className="relative z-30">
         {/* Layers Toggle */}
-        <Button
-          type="button"
-          variant="white"
-          className={cn('absolute top-0 -mt-px h-10 border-l-0 !py-3', {
-            'hidden md:flex': true,
-            'px-1': isLayersOpen,
-            'border-l border-black px-3': !isLayersOpen,
-            '-ml-px': isSidebarOpen && !isLayersOpen,
-          })}
-          onClick={() => setLayersOpen(!isLayersOpen)}
-        >
-          {isLayersOpen && (
-            <>
-              <LuChevronLeft className="-ml-px h-6 w-6" aria-hidden />
-              <span className="sr-only">Close layers</span>
-            </>
-          )}
-          {!isLayersOpen && (
-            <>
-              <Icon icon={LayersIcon} className="ml-0.5 h-4 w-4 pb-px" />
-              <span className="pr-1 pl-3 font-mono text-xs font-semibold">Layers</span>
-            </>
-          )}
-        </Button>
+        {showLayersToggle && (
+          <Button
+            type="button"
+            variant="white"
+            className={cn('absolute top-0 -mt-px h-10 border-l-0 !py-3', {
+              'hidden md:flex': true,
+              'px-1': isLayersOpen,
+              'border-l border-black px-3': !isLayersOpen,
+              '-ml-px': (isSidebarOpen && !isLayersOpen) || (!isSidebarOpen && !isLayersOpen),
+            })}
+            onClick={() => setLayersOpen(!isLayersOpen)}
+          >
+            {isLayersOpen && (
+              <>
+                <LuChevronLeft className="-ml-px h-6 w-6" aria-hidden />
+                <span className="sr-only">Close layers</span>
+              </>
+            )}
+            {!isLayersOpen && (
+              <>
+                <Icon icon={LayersIcon} className="ml-0.5 h-4 w-4 pb-px" />
+                <span className="pr-1 pl-3 font-mono text-xs font-semibold">Layers</span>
+              </>
+            )}
+          </Button>
+        )}
 
         {/* Sidebar Toggle */}
-        {!(isSidebarOpen && isLayersOpen) && (
+        {showSidebarToggle && (
           <Button
             type="button"
             variant="white"
