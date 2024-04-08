37f0e7fc0fa1940649df668c28e10dbf99acd309
Andrés González <andres.gonzalez@vizzuality.com>
2024-04-08 09:50:53 +0200

    conservation builder: existing PA

---
+++ frontend/src/components/charts/stacked-horizontal-bar-chart/index.tsx
@@ -0,0 +1,85 @@
+import TooltipButton from '@/components/tooltip-button';
+import { cn } from '@/lib/classnames';
+import { formatPercentage, formatKM } from '@/lib/utils/formats';
+
+const DEFAULT_MAX_PERCENTAGE = 100;
+
+type StackedHorizontalBarChartProps = {
+  className: string;
+  title?: string;
+  info?: string;
+  totalProtectedArea: number;
+  totalArea: number;
+  highlightedPercentage: number;
+  data: {
+    background: string;
+    total: number;
+    totalPercentage: number;
+  }[];
+  showLegend?: boolean;
+  showTarget?: boolean;
+};
+
+const StackedHorizontalBarChart: React.FC<StackedHorizontalBarChartProps> = ({
+  className,
+  data,
+  title,
+  info,
+  totalProtectedArea,
+  totalArea,
+  highlightedPercentage,
+  showLegend = true,
+  showTarget = true,
+}) => {
+  return (
+    <div className={cn('font-mono', className)}>
+      <div className="flex items-end justify-end text-3xl font-bold">
+        {formatPercentage(highlightedPercentage, { displayPercentageSign: false })}
+        <span className="pb-1.5 pl-1 text-xs">%</span>
+      </div>
+      <div className="flex justify-between text-xs">
+        <span className="flex items-center">
+          {title && title}
+          {info && <TooltipButton text={info} />}
+        </span>
+        <span>
+          {formatKM(totalProtectedArea)} km<sup>2</sup> out of {formatKM(totalArea)} km<sup>2</sup>
+        </span>
+      </div>
+      <div className="relative my-2 flex h-3.5">
+        <span className="absolute top-1/2 h-px w-full border-b border-dashed border-black"></span>
+        {data.map((item, index) => (
+          <span
+            key={index}
+            className="absolute top-0 bottom-0 left-0  !bg-cover"
+            style={{
+              background: item.background,
+              width: `${item.totalPercentage}%`,
+              ...(index > 0 && { left: `${data[index - 1].totalPercentage}%` }),
+            }}
+          />
+        ))}
+        {showTarget && (
+          <span
+            className="absolute top-0 bottom-0 left-[30%] w-1 border-x border-white bg-orange"
+            // style={{
+            //   left: '30%',
+            // }}
+          >
+            <span className="absolute right-0 top-5 whitespace-nowrap text-xs text-orange">
+              30% target
+            </span>
+          </span>
+        )}
+      </div>
+      {showLegend && (
+        <div className="flex justify-between text-xs">
+          <span>0%</span>
+          <span>{DEFAULT_MAX_PERCENTAGE}%</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StackedHorizontalBarChart;
--- frontend/src/containers/map/sidebar/main-panel/modelling/widget/index.tsx
+++ frontend/src/containers/map/sidebar/main-panel/modelling/widget/index.tsx
@@ -1,21 +1,24 @@
 import { PropsWithChildren, useMemo } from 'react';
 
+import theme from 'lib/tailwind';
+
+import { useQueries } from '@tanstack/react-query';
 import { useAtomValue } from 'jotai';
 
-import HorizontalBarChart from '@/components/charts/horizontal-bar-chart';
+import StackedHorizontalBarChart from '@/components/charts/stacked-horizontal-bar-chart';
 import TooltipButton from '@/components/tooltip-button';
 import Widget from '@/components/widget';
 import { modellingAtom } from '@/containers/map/store';
 import { cn } from '@/lib/classnames';
-import { useGetLocations } from '@/types/generated/location';
+import {
+  getGetProtectionCoverageStatsQueryOptions,
+  useGetProtectionCoverageStats,
+} from '@/types/generated/protection-coverage-stat';
+import { Location } from '@/types/generated/strapi.schemas';
 
 import useTooltips from '../useTooltips';
 
 const DEFAULT_ENTRY_CLASSNAMES = 'border-t border-black py-6';
-
-const DEFAULT_CHART_DATA = {
-  background: '#000',
-};
 
 const DEFAULT_CHART_PROPS = {
   className: 'py-2',
@@ -37,6 +40,24 @@ const WidgetSectionWidgetTitle: React.FC<WidgetSectionWidgetTitleProps> = ({ tit
   );
 };
 
+const WidgetLegend: React.FC = () => {
+  const LEGEND_LINE_CLASSES =
+    'relative pl-9 font-mono text-xs before:absolute before:left-0 before:top-1/2 before:h-[2px] before:w-[28px] before:-translate-y-1/2';
+
+  return (
+    <ul>
+      <li>
+        <span className={cn(LEGEND_LINE_CLASSES, 'before:bg-green')}>
+          Existing marine conservation coverage
+        </span>
+      </li>
+      <li>
+        <span className={cn(LEGEND_LINE_CLASSES, 'before:bg-black')}>New added area</span>
+      </li>
+    </ul>
+  );
+};
+
 const ModellingWidget: React.FC = () => {
   const chartsProps = DEFAULT_CHART_PROPS;
 
@@ -49,70 +70,169 @@ const ModellingWidget: React.FC = () => {
   // Tooltips with mapping
   const tooltips = useTooltips();
 
-  // Get all locations in order to get country names for modelling data
-  const { data: locationsData } = useGetLocations(
+  const {
+    data: globalProtectionStatsData,
+  }: {
+    data: {
+      protectedArea: number;
+      percentageProtectedArea: number;
+      totalMarineArea: number;
+      totalProtectedArea: number;
+      totalPercentage: number;
+      totalCustomAreas: number;
+      totalExistingAreaPercentage: number;
+      totalCustomAreasPercentage: number;
+    };
+  } = useGetProtectionCoverageStats(
     {
-      'pagination[limit]': -1,
-      sort: 'name:asc',
+      filters: {
+        location: {
+          code: 'GLOB',
+        },
+      },
+      populate: '*',
+      // @ts-expect-error this is an issue with Orval typing
+      'sort[year]': 'desc',
+      'pagination[limit]': 2,
     },
     {
       query: {
-        placeholderData: { data: [] },
-        select: ({ data }) => data,
+        enabled: Boolean(modellingData?.locations_area),
+        select: ({ data }) => {
+          if (!data) return null;
+
+          const protectedArea = data.reduce(
+            (acc, entry) => acc + entry.attributes.cumSumProtectedArea,
+            0
+          );
+
+          const totalMarineArea =
+            data?.[0].attributes?.location?.data?.attributes?.totalMarineArea || 0;
+
+          const totalCustomAreas = modellingData.locations_area.reduce((acc, location) => {
+            return acc + location.protected_area;
+          }, 0);
+
+          const totalProtectedArea = protectedArea + totalCustomAreas;
+          //  ? percentage of custom protected areas (analysis)
+          const totalCustomAreasPercentage = (totalCustomAreas / totalMarineArea) * 100;
+          //  ? percentage of existing global protected area
+          const totalExistingAreaPercentage = (protectedArea / totalMarineArea) * 100;
+
+          const totalPercentage = totalCustomAreasPercentage + totalExistingAreaPercentage;
+
+          return {
+            protectedArea,
+            percentageProtectedArea: (protectedArea / totalMarineArea) * 100,
+            totalMarineArea,
+            totalProtectedArea,
+            totalPercentage,
+            totalCustomAreas,
+            totalExistingAreaPercentage,
+            totalCustomAreasPercentage,
+          };
+        },
+        refetchOnWindowFocus: false,
       },
     }
   );
 
-  // Build contribution details details for the charts
-  const contributionDetailsData = useMemo(() => {
-    if (!locationsData.length || !modellingData) return null;
+  const locationQueries = useQueries({
+    queries: (modellingData?.locations_area || []).map((location) =>
+      getGetProtectionCoverageStatsQueryOptions(
+        {
+          filters: {
+            location: {
+              code: location.code,
+            },
+          },
+          populate: '*',
+          // @ts-expect-error this is an issue with Orval typing
+          'sort[year]': 'desc',
+          'pagination[limit]': 2,
+        },
+        {
+          query: {
+            enabled: Boolean(modellingData?.locations_area),
+            select: ({ data }) => {
+              if (!data) return null;
 
-    const modellingLocations = modellingData?.locations_area;
+              const protectedArea = data.reduce(
+                (acc, entry) => acc + entry.attributes.cumSumProtectedArea,
+                0
+              );
 
-    const chartData = modellingLocations.map((modellingLocation) => {
-      const location = locationsData?.find(
-        ({ attributes }) => modellingLocation.code === attributes?.code
-      )?.attributes;
+              // ? total extension of location
+              const totalMarineArea =
+                data?.[0].attributes?.location?.data?.attributes?.totalMarineArea || 0;
 
-      if (!location) return null;
+              // ? total custom  protected area (analysis)
+              const totalCustomArea = modellingData.locations_area.find(
+                ({ code }) => code === data?.[0].attributes?.location.data?.attributes?.code
+              ).protected_area;
 
-      const { name, totalMarineArea } = location;
+              //  ? percentage of custom protected area (analysis)
+              const totalCustomAreaPercentage = (totalCustomArea / totalMarineArea) * 100;
+              //  ? percentage of existing protected area
+              const totalExistingAreaPercentage = (protectedArea / totalMarineArea) * 100;
 
-      return {
-        ...DEFAULT_CHART_DATA,
-        title: name,
-        protectedArea: modellingLocation.protected_area,
-        totalArea: totalMarineArea,
-      };
-    });
+              // ? sum of existing protected area and protected custom area (analysis)
+              const totalProtectedArea = protectedArea + totalCustomArea;
 
-    return chartData;
-  }, [modellingData, locationsData]);
+              const totalPercentage = totalCustomAreaPercentage + totalExistingAreaPercentage;
 
-  // Build global contribution details for the charts
-  const globalContributionData = useMemo(() => {
-    const location = locationsData?.find(
-      ({ attributes }) => attributes?.code === 'GLOB'
-    )?.attributes;
-
-    if (!location) return null;
-
-    return {
-      ...DEFAULT_CHART_DATA,
-      protectedArea: modellingData?.total_protected_area,
-      totalArea: location?.totalMarineArea,
-    };
-  }, [modellingData, locationsData]);
-
-  const administrativeBoundaries = contributionDetailsData?.map(({ title }) => title);
+              return {
+                location: data?.[0].attributes?.location?.data?.attributes,
+                totalMarineArea,
+                totalProtectedArea,
+                protectedArea,
+                totalExistingAreaPercentage,
+                totalCustomArea,
+                totalCustomAreaPercentage,
+                totalPercentage,
+              };
+            },
+            refetchOnWindowFocus: false,
+          },
+        }
+      )
+    ),
+  });
 
   const loading = modellingStatus === 'running';
   const error = modellingStatus === 'error';
 
+  // @ts-expect-error will check later
+  const nationalLevelContributions: {
+    location: Location;
+    percentageProtectedArea: number;
+    totalMarineArea: number;
+    totalProtectedArea: number;
+    protectedArea: number;
+    totalExistingAreaPercentage: number;
+    totalCustomArea: number;
+    totalCustomAreaPercentage: number;
+    totalPercentage: number;
+  }[] = useMemo(
+    () =>
+      locationQueries
+        .map((query) => {
+          if (['loading', 'error'].includes(query.status)) return null;
+
+          return query.data;
+        })
+        .filter((d) => Boolean(d)),
+    [locationQueries]
+  );
+
+  const administrativeBoundaries = nationalLevelContributions?.map(
+    (contribution) => contribution?.location?.name
+  );
+
   return (
     <Widget
       className="border-b border-black py-0"
-      noData={!contributionDetailsData}
+      noData={!nationalLevelContributions}
       loading={loading}
       error={error}
       messageError={messageError}
@@ -129,20 +249,66 @@ const ModellingWidget: React.FC = () => {
           </span>
         </div>
         <div className={cn(DEFAULT_ENTRY_CLASSNAMES)}>
-          <WidgetSectionWidgetTitle
-            title="Contribution details"
-            tooltip={tooltips?.['contributionDetails']}
-          />
-          {contributionDetailsData?.map((entry) => (
-            <HorizontalBarChart key={entry.title} data={entry} {...chartsProps} />
-          ))}
+          <div className="space-y-2">
+            <WidgetSectionWidgetTitle
+              title="National level contribution"
+              tooltip={tooltips?.['contributionDetails']}
+            />
+            <WidgetLegend />
+          </div>
+          {nationalLevelContributions?.map((contribution) => {
+            return (
+              <StackedHorizontalBarChart
+                key={contribution?.location?.code}
+                title={contribution?.location?.name}
+                totalProtectedArea={contribution?.totalProtectedArea}
+                totalArea={contribution?.totalMarineArea}
+                highlightedPercentage={contribution?.totalPercentage}
+                data={[
+                  {
+                    background: theme.colors.green as string,
+                    total: contribution?.protectedArea,
+                    totalPercentage: contribution?.totalExistingAreaPercentage,
+                  },
+                  {
+                    background: theme.colors.black as string,
+                    total: contribution?.totalCustomArea,
+                    totalPercentage: contribution?.totalCustomAreaPercentage,
+                  },
+                ]}
+                {...chartsProps}
+              />
+            );
+          })}
         </div>
         <div className={cn(DEFAULT_ENTRY_CLASSNAMES)}>
-          <WidgetSectionWidgetTitle
-            title="Global contribution"
-            tooltip={tooltips?.['globalContribution']}
+          <div className="space-y-2">
+            <WidgetSectionWidgetTitle
+              title="Global level contribution"
+              tooltip={tooltips?.['globalContribution']}
+            />
+            <WidgetLegend />
+          </div>
+          <StackedHorizontalBarChart
+            title="Global"
+            totalProtectedArea={globalProtectionStatsData?.totalProtectedArea}
+            totalArea={globalProtectionStatsData?.totalMarineArea}
+            highlightedPercentage={globalProtectionStatsData?.totalPercentage}
+            data={[
+              {
+                background: theme.colors.green as string,
+                total: globalProtectionStatsData?.protectedArea,
+                totalPercentage: globalProtectionStatsData?.totalExistingAreaPercentage,
+              },
+              {
+                background: theme.colors.black as string,
+                total: globalProtectionStatsData?.totalCustomAreas,
+                totalPercentage: globalProtectionStatsData?.totalCustomAreasPercentage,
+              },
+            ]}
+            {...chartsProps}
+            showTarget
           />
-          <HorizontalBarChart data={globalContributionData} {...chartsProps} />
         </div>
       </div>
     </Widget>
