86371419ca4b228fd6a1c964325ea7d6b279c19c
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-05-29 13:46:44 +0100

    Revert changes to the pages object constant - no longer needed

--- frontend/src/constants/pages.ts
+++ frontend/src/constants/pages.ts
@@ -1,24 +1,8 @@
-/**
- * NOTE: The reason for the separation of tools and static pages, is for Typescript purposes.
- * In certain cases (eg: <LocationSelector />) we need to validate that the target page passed
- * as a prop is actually a tool page (progress tracker, modelling tool) as static pages won't
- * support the parameters that will be passed to them by the location selector. For general
- * purposes PAGES can be used instead, as usual.
- */
-
-export const TOOLS_PAGES = {
+export const PAGES = {
   progressTracker: '/progress-tracker',
   conservationBuilder: '/conservation-builder',
-} as const;
-
-export const STATIC_PAGES = {
   homepage: '/',
   knowledgeHub: '/knowledge-hub',
   contact: '/contact',
   about: '/about',
-} as const;
-
-export const PAGES = {
-  ...TOOLS_PAGES,
-  ...STATIC_PAGES,
 } as const;
