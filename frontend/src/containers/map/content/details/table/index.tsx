106a0f6d658ba104bf5a843514adfa161df4d08a
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-06-13 12:14:16 +0100

    Fix double border display between table headers and first row

--- frontend/src/containers/map/content/details/table/index.tsx
+++ frontend/src/containers/map/content/details/table/index.tsx
@@ -72,13 +72,13 @@ const MapTable = ({ columns, data }) => {
       </thead>
       <tbody>
         {hasData &&
-          table.getRowModel().rows.map((row) => {
+          table.getRowModel().rows.map((row, idx) => {
             return (
               <tr
                 key={row.id}
                 className={cn({
                   'border-b border-t border-black': true,
-                  'border-t-0': row.index === 0,
+                  'border-t-0': idx === 0,
                 })}
               >
                 {row.getVisibleCells().map((cell) => {
