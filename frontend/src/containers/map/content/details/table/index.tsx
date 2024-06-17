b3198345c66a1a33af0a3d8f4caf152e7b9ad528
Simao Rodrigues <arodrigues.simao@gmail.com>
2024-06-17 11:22:53 +0100

    Details tables to support definition of column separators + setup for static map cells
    message aborts the commit. # # Date:  Mon Jun 17 11:22:53 2024 +0100 # # interactive rebase in progress; onto
    b566b652 # Last command done (1 command done): # reword fdcf016d ONGOING # No commands remaining. # You are
    currently editing a commit while rebasing branch
    'SKY30-374-fe-hierarchical-data-table-to-simplify-layout-and-combine-duplicate-records' on 'b566b652'. # #
    Changes to be committed: # modified:  frontend/src/containers/map/content/details/table/index.tsx # modified:
    frontend/src/containers/map/content/details/tables/national-highseas/index.tsx # modified:
    frontend/src/containers/map/content/details/tables/national-highseas/useColumns.tsx #

--- frontend/src/containers/map/content/details/table/index.tsx
+++ frontend/src/containers/map/content/details/table/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 
 import {
   flexRender,
@@ -15,7 +15,7 @@ import { FCWithMessages } from '@/types';
 // ! todo: type columns,data properly
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const MapTable: FCWithMessages = ({ columns, data }) => {
+const MapTable: FCWithMessages = ({ columns, data, columnSeparators = null }) => {
   const t = useTranslations('containers.map');
 
   const tableRef = useRef<HTMLTableElement>();
@@ -41,8 +41,15 @@ const MapTable: FCWithMessages = ({ columns, data }) => {
   const hasData = table.getRowModel().rows?.length > 0;
 
   const firstColumn = columns[0];
-  const secondColumn = columns[1];
   const lastColumn = columns[columns.length - 1];
+
+  const shouldAddColumnSeparator = useCallback(
+    (columnId) => {
+      const isFirstColumn = columnId === firstColumn.accessorKey;
+      return columnSeparators ? columnSeparators?.includes(columnId) : isFirstColumn;
+    },
+    [columnSeparators, firstColumn.accessorKey]
+  );
 
   return (
     <table
@@ -53,19 +60,20 @@ const MapTable: FCWithMessages = ({ columns, data }) => {
         {table.getHeaderGroups().map((headerGroup) => (
           <tr key={headerGroup.id} className="shadow-[0_2px_0_-1px_rgb(0,0,0)]">
             {headerGroup.headers.map((header) => {
-              const { id, column, index } = header;
+              const { id, column } = header;
               const isFirstColumn = id === firstColumn.accessorKey;
-              const isSecondColumn = index === 1;
               const isLastColumn = id === lastColumn.accessorKey;
+              const isMapColumn = column.id === 'map';
 
               return (
                 <th
                   key={id}
                   ref={isFirstColumn ? firstColumnRef : null}
                   className={cn({
-                    'h-10 py-3 pl-6 pr-16': true,
+                    'border-r border-dashed border-black': shouldAddColumnSeparator(id),
+                    'h-10': true,
+                    'pl-6 pr-16': !isMapColumn,
                     'pl-0 pr-5': isFirstColumn,
-                    'border-l border-dashed border-black': isSecondColumn,
                     'pr-0': isLastColumn,
                   })}
                 >
@@ -96,17 +104,19 @@ const MapTable: FCWithMessages = ({ columns, data }) => {
                 {row.getVisibleCells().map((cell) => {
                   const { column } = cell;
                   const isFirstColumn = column.id === firstColumn.accessorKey;
-                  const isSecondColumn = column.id === secondColumn.accessorKey;
                   const isLastColumn = column.id === lastColumn.accessorKey;
+                  const isMapColumn = column.id === 'map';
 
                   return (
                     <td
                       key={cell.id}
                       className={cn({
-                        'h-16 py-3 pl-6 pr-16': true,
+                        'h-16 pl-6': true,
+                        'pl-6 pr-16 ': !isMapColumn,
+                        '-mt-px -mb-px': isMapColumn,
                         'pl-0 pr-5': isFirstColumn,
-                        'border-l border-dashed border-black': isSecondColumn,
                         'pr-0': isLastColumn,
+                        'border-r border-dashed border-black': shouldAddColumnSeparator(column.id),
                       })}
                     >
                       {flexRender(column.columnDef.cell, cell.getContext())}
