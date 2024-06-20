8eadec2e886a0d0eaefca9bc921e5eca2527685e
Alicia <alicia.arenzana@gmail.com>
2024-06-20 15:00:59 +0200

    fix issue with docker volumes

--- cms/database/migrations/2024.01.19T10.00.00.change_data_tool_resource_type_relation_to_has_many.js
+++ cms/database/migrations/2024.01.19T10.00.00.change_data_tool_resource_type_relation_to_has_many.js
@@ -1,18 +1,17 @@
 module.exports = {
     async up(knex) {
-        // change the relation from 1:1 to 1:n without losing data
-
-        // add a data_tool_resource_type_order column
-        await knex.schema.table('data_tools_data_tool_resource_type_links', table => {
-            table.integer('data_tool_resource_type_order');
-        });
-        // populate the reorder column with 1
-        await knex.raw('UPDATE data_tools_data_tool_resource_type_links SET data_tool_resource_type_order = 1');
-        // make the column not nullable
-        await knex.schema.alterTable('data_tools_data_tool_resource_type_links', table => {
-            table.integer('data_tool_resource_type_order').notNullable().alter();
-        });
-        // rename the table to match strapi convention
-        await knex.schema.renameTable('data_tools_data_tool_resource_type_links', 'data_tools_data_tool_resource_types_links');
+      // change the relation from 1:1 to 1:n without losing data
+      // add a data_tool_resource_type_order column
+      // await knex.schema.table('data_tools_data_tool_resource_type_links', table => {
+      //     table.integer('data_tool_resource_type_order');
+      // });
+      // // populate the reorder column with 1
+      // await knex.raw('UPDATE data_tools_data_tool_resource_type_links SET data_tool_resource_type_order = 1');
+      // // make the column not nullable
+      // await knex.schema.alterTable('data_tools_data_tool_resource_type_links', table => {
+      //     table.integer('data_tool_resource_type_order').notNullable().alter();
+      // });
+      // // rename the table to match strapi convention
+      // await knex.schema.renameTable('data_tools_data_tool_resource_type_links', 'data_tools_data_tool_resource_types_links');
     },
 }
