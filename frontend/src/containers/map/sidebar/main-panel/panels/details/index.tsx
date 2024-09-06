bf03222951e6a996a66fe510f746e66b1e59eb26
Clément Prod'homme <contact@clementprodhomme.fr>
2024-09-06 16:18:45 +0200

    Store the tab selection in the URL

--- frontend/src/containers/map/sidebar/main-panel/panels/details/index.tsx
+++ frontend/src/containers/map/sidebar/main-panel/panels/details/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { useRouter } from 'next/router';
 
@@ -8,7 +8,6 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { PAGES } from '@/constants/pages';
 import { useMapSearchParams } from '@/containers/map/content/map/sync-settings';
 import { useSyncMapContentSettings } from '@/containers/map/sync-settings';
-import { cn } from '@/lib/classnames';
 import { FCWithMessages } from '@/types';
 import { useGetLocations } from '@/types/generated/location';
 
@@ -26,8 +25,9 @@ const SidebarDetails: FCWithMessages = () => {
     push,
     query: { locationCode = 'GLOB' },
   } = useRouter();
-  const [{ showDetails }] = useSyncMapContentSettings();
   const searchParams = useMapSearchParams();
+
+  const [{ tab }, setSettings] = useSyncMapContentSettings();
 
   const { data: locationsData } = useGetLocations({
     locale,
@@ -44,13 +44,21 @@ const SidebarDetails: FCWithMessages = () => {
     }));
   }, [locationsData?.data]);
 
-  const handleLocationSelected = (locationCode) => {
-    push(`${PAGES.progressTracker}/${locationCode}?${searchParams.toString()}`);
-  };
+  const handleLocationSelected = useCallback(
+    (locationCode) => {
+      push(`${PAGES.progressTracker}/${locationCode}?${searchParams.toString()}`);
+    },
+    [push, searchParams]
+  );
+
+  const handleTabChange = useCallback(
+    (tab: string) => setSettings((prevSettings) => ({ ...prevSettings, tab })),
+    [setSettings]
+  );
 
   return (
-    <Tabs className="h-full w-full">
-      <div className="sticky border-b border-black bg-orange px-4 pt-4 md:px-8 md:pt-6">
+    <Tabs value={tab} onValueChange={handleTabChange} className="flex h-full w-full flex-col">
+      <div className="shrink-0 border-b border-black bg-orange px-4 pt-4 md:px-8 md:pt-6">
         <h1 className="text-5xl font-black">{locationsData?.data[0]?.attributes?.name}</h1>
         <LocationSelector className="mt-2" theme="orange" onChange={handleLocationSelected} />
         <CountriesList className="mt-2" bgColorClassName="bg-orange" countries={memberCountries} />
@@ -61,12 +69,7 @@ const SidebarDetails: FCWithMessages = () => {
         </TabsList>
         {/* <DetailsButton className="mt-4" /> */}
       </div>
-      <div
-        className={cn({
-          'h-[calc(100%-161px)] overflow-y-auto': true,
-          'h-full': showDetails,
-        })}
-      >
+      <div className="flex-grow overflow-y-auto">
         <TabsContent value="summary">{t('summary')}</TabsContent>
         <TabsContent value="terrestrial">{t('terrestrial')}</TabsContent>
         <TabsContent value="marine">
--- frontend/src/containers/map/sync-settings.ts
+++ frontend/src/containers/map/sync-settings.ts
@@ -1,10 +1,9 @@
 import { useQueryState } from 'next-usequerystate';
 import { parseAsJson } from 'next-usequerystate/parsers';
 
-const DEFAULT_SYNC_CONTENT_SETTINGS: {
-  showDetails: boolean;
-} = {
+const DEFAULT_SYNC_CONTENT_SETTINGS = {
   showDetails: false,
+  tab: 'summary',
 };
 
 export const useSyncMapContentSettings = () => {
