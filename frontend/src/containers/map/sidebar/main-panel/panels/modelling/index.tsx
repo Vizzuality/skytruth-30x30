3b116efc313ce735b42f63d86da5e93c219cfe66
Clément Prod'homme <contact@clementprodhomme.fr>
2024-10-08 10:20:10 +0200

    Remove the location input and URL param

--- frontend/src/containers/map/sidebar/main-panel/panels/modelling/index.tsx
+++ frontend/src/containers/map/sidebar/main-panel/panels/modelling/index.tsx
@@ -1,13 +1,9 @@
 import { useCallback, useEffect, useMemo, useRef } from 'react';
 
-import { useRouter } from 'next/router';
-
 import { useAtomValue } from 'jotai';
 import { useTranslations } from 'next-intl';
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { PAGES } from '@/constants/pages';
-import { useMapSearchParams } from '@/containers/map/content/map/sync-settings';
 import { modellingAtom } from '@/containers/map/store';
 import { useSyncMapContentSettings } from '@/containers/map/sync-settings';
 import useScrollPosition from '@/hooks/use-scroll-position';
@@ -26,19 +22,10 @@ const SidebarModelling: FCWithMessages = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const containerScroll = useScrollPosition(containerRef);
 
-  const { push } = useRouter();
-  const searchParams = useMapSearchParams();
   const { status: modellingStatus } = useAtomValue(modellingAtom);
   const [{ tab }, setSettings] = useSyncMapContentSettings();
 
   const showIntro = useMemo(() => modellingStatus === 'idle', [modellingStatus]);
-
-  const handleLocationSelected = useCallback(
-    (locationCode) => {
-      push(`${PAGES.conservationBuilder}/${locationCode}?${searchParams.toString()}`);
-    },
-    [push, searchParams]
-  );
 
   const handleTabChange = useCallback(
     (tab: string) => setSettings((prevSettings) => ({ ...prevSettings, tab })),
@@ -62,13 +49,7 @@ const SidebarModelling: FCWithMessages = () => {
   return (
     <Tabs value={tab} onValueChange={handleTabChange} className="flex h-full w-full flex-col">
       <div className="flex flex-shrink-0 flex-col gap-y-2 border-b border-black bg-blue-600 px-4 pt-4 md:px-8 md:pt-6">
-        <div
-          className={cn({
-            'flex gap-y-2 gap-x-5': true,
-            'flex-col': containerScroll === 0,
-            'flex-row': containerScroll > 0,
-          })}
-        >
+        <div>
           <h1
             className={cn({
               'text-ellipsis font-black transition-all': true,
@@ -78,12 +59,6 @@ const SidebarModelling: FCWithMessages = () => {
           >
             {showIntro ? t('conservation-scenarios') : t('custom-area')}
           </h1>
-          <LocationSelector
-            className="flex-shrink-0"
-            theme="blue"
-            size={containerScroll > 0 ? 'small' : 'default'}
-            onChange={handleLocationSelected}
-          />
         </div>
         {!showIntro && <p className="mt-2 font-black">{t('custom-area-description')}</p>}
         <TabsList className="relative top-px mt-5">
