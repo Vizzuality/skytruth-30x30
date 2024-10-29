1a9a155a7278351afed177d34e129c7cdf7ff2ab
Clément Prod'homme <contact@clementprodhomme.fr>
2024-10-29 15:11:22 +0100

    fix(dialog): Improve responsive styles

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -40,7 +40,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-auto max-w-[600px] translate-x-[-50%] translate-y-[-50%] gap-6 border border-black bg-white p-10 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
+        'fixed left-[50%] top-[50%] z-50 grid max-h-[calc(100%_-_theme(spacing.8))] w-[calc(100%_-_theme(spacing.8))] max-w-[600px] -translate-x-1/2 -translate-y-1/2 gap-6 overflow-auto border border-black bg-white p-10 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] lg:w-auto',
         className
       )}
       {...props}
